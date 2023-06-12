3fbfb25cd916f322af3dc8aaee3bcf228ff7f4c7
Daviduxx <borotto.davide@gmail.com>
2023-06-12 17:00:52 +0200

    components

--- CDK_Pizza/src/app/counter/counter.component.ts
+++ CDK_Pizza/src/app/counter/counter.component.ts
@@ -5,6 +5,39 @@ import { Component } from '@angular/core';
   templateUrl: './counter.component.html',
   styleUrls: ['./counter.component.scss']
 })
-export class CounterComponent {
+
+export class CounterComponent{
+
+  pizzaCounter:number = 0;
+  awCounter:number = 0;
+  customerCounter:number = 0;
+  staffCounter:number = 0;
+
+  pizzaCounterFn = setInterval(() => {
+    this.pizzaCounter++;
+    if(this.pizzaCounter == 100){
+      clearInterval(this.pizzaCounterFn)
+    }
+  },100)
+
+  awCounterFn = setInterval(() => {
+    this.awCounter++;
+    if(this.awCounter == 85){
+      clearInterval(this.awCounterFn)
+    }
+  },100)
+  customerCounterFn = setInterval(() => {
+    this.customerCounter+= 10;
+    if(this.customerCounter == 10500){
+      clearInterval(this.customerCounterFn)
+    }
+  },10)
+  staffCounterFn = setInterval(() => {
+    this.staffCounter++;
+    if(this.staffCounter == 900){
+      clearInterval(this.staffCounterFn)
+    }
+  },10)
+
 
 }
