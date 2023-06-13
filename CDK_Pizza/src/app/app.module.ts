a915cdb1c9feefed1554e12be65c7c7759a7df7e
DamianiCristiano <damiani.cristiano96@gmail.com>
2023-06-13 09:24:12 +0200

    push cristiano

--- CDK_Pizza/src/app/app.module.ts
+++ CDK_Pizza/src/app/app.module.ts
@@ -4,10 +4,16 @@ import { BrowserModule } from '@angular/platform-browser';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { NgbModule } from '@ng-bootstrap/ng-bootstrap';
+import { OurMenuPricingComponent } from './component/our-menu-pricing/our-menu-pricing.component';
+import { HotPizzaMealsComponent } from './component/hot-pizza-meals/hot-pizza-meals.component';
+
+
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    OurMenuPricingComponent,
+    HotPizzaMealsComponent
   ],
   imports: [
     BrowserModule,
---
+++ CDK_Pizza/src/app/component/our-menu-pricing/our-menu-pricing.component.ts
@@ -0,0 +1,10 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-our-menu-pricing',
+  templateUrl: './our-menu-pricing.component.html',
+  styleUrls: ['./our-menu-pricing.component.scss']
+})
+export class OurMenuPricingComponent {
+
+}
